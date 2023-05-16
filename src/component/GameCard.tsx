d7153e91469db45adcabee9d157fea2b2075fa48
zubairbabar <max_min26@hotmail.com>
2023-05-16 14:02:44 +0400

    show loading skeltons

--- src/component/GameCard.tsx
+++ src/component/GameCard.tsx
@@ -1,5 +1,5 @@
 import { Game } from "./hooks/useGames";
-import { Card, CardBody, HStack, Heading, Image, Text } from "@chakra-ui/react";
+import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 interface Props {
@@ -7,7 +7,7 @@ interface Props {
 }
 const GameCard = ({ game }: Props) => {
   return (
-    <Card borderRadius={10} overflow="hidden">
+    <Card width={"300px"} borderRadius={10} overflow="hidden">
       <Image src={game.background_image}></Image>
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
